fafbcfb9e909a78252104e0c5d1bffc3a5a92151
Pavel Marek <pavel.marek@oracle.com>
2019-10-28 15:49:28 +0100

    Remove nonsential R-tests.

--- tests/main.R
+++ tests/main.R
@@ -1,34 +1,5 @@
 stopifnot(require(altreptests))
 
-EXPECTED_LENGTH <- 5
-
-construct <- function() {
-    return (.Call("ctor", PACKAGE="samplepackage"))
-}
-
-test_length <- function() {
-    vec <- construct()
-    stopifnot( length(vec) == EXPECTED_LENGTH)
-}
-
-test_data <- function() {
-    vec <- construct()
-    for (i in EXPECTED_LENGTH) {
-        stopifnot( vec[i] == 0);
-    }
-}
-
-test_modification <- function() {
-    vec <- construct()
-    # In this location the altvec gets replaced by normal vector (materialisation occurs).
-    # TODO: How to avoid materialisation in ALTREP?
-    vec[2] <- 42
-
-    vec[3] <- 23
-    expected <- c (0, 42, 23, 0, 0)
-    stopifnot( expected == vec)
-}
-
 run_all_tests <- function() {
     for (test_func_tuple in list(
                         list("test_length", test_length),
@@ -47,9 +18,13 @@ run_all_tests <- function() {
 }
 
 
-run_all_C_tests <- function() {
-    .Call("run_tests")
+test_mmap <- function() {
+    stopifnot(require(simplemmap))
+    fname <- tempfile()
+    data <- runif(20)
+    writeBin(data, fname)
+
+    mmaped_data <- mmap(fname)
+    munmap(mmaped_data)
 }
 
-
-run_all_C_tests()
