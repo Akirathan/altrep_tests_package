8ff70e3e396a171f2f1692e5c89d35cd93decd6e
Pavel Marek <pavel.marek@oracle.com>
2019-10-28 17:22:14 +0100

    Add test_get_regions tests

--- src/class_tests.c
+++ src/class_tests.c
@@ -11,11 +11,15 @@ static SEXP _new_instance();
 static void _test_elt();
 static void _test_dataptr();
 static void _test_length_method();
+static void _test_get_one_region();
+static void _test_get_more_regions();
 
 static const test_t _tests[] = {
     {"test_elt", _test_elt},
     {"test_dataptr", _test_dataptr},
     {"test_length_method", _test_length_method},
+    {"test_get_one_region", _test_get_one_region},
+    {"test_get_more_regions", _test_get_more_regions},
     {NULL, NULL}
 };
 
@@ -114,4 +118,73 @@ static void _test_length_method()
 {
     SEXP instance = _new_instance();
     CHECK_MSG (LENGTH(instance) > 0, "Length should be > 0");
+}
+
+static void _test_get_one_region()
+{
+    SEXP instance = PROTECT(_new_instance());
+
+    if (TYPEOF(instance) != INTSXP) {
+        warning("\t%s for type %s not yet implemented.\n", __func__, type2char(TYPEOF(instance)));
+        goto end;
+    }
+    if (LENGTH(instance) < 5) {
+        warning("Expected at least length 5 of instance.\n");
+        goto end;
+    }
+    SET_INTEGER_ELT(instance, 1, 1);
+    SET_INTEGER_ELT(instance, 2, 2);
+
+    int buf[2] = {0};
+    INTEGER_GET_REGION(instance, 1, 2, buf);
+    CHECK( buf[0] == 1);
+    CHECK( buf[1] == 2);
+
+end:
+    UNPROTECT(1);
+}
+
+static void _test_get_more_regions()
+{
+    //  0 1 2 3 4 5 6 7 8 9
+    // [x 1 1 1 x x x 2 2 2]
+    const int region_1_from = 1;
+    const int region_1_size = 3;
+    const int region_1_value = 1;
+
+    const int region_2_from = 8;
+    const int region_2_size = 3;
+    const int region_2_value = 2;
+
+    SEXP instance = PROTECT(_new_instance());
+
+    if (TYPEOF(instance) != INTSXP) {
+        warning("\t%s for type %s not yet implemented.\n", __func__, type2char(TYPEOF(instance)));
+        goto end;
+    }
+    if (LENGTH(instance) < 10) {
+        warning("Expected at least length 10 of instance.\n");
+        goto end;
+    }
+
+    for (int i = region_1_from; i < region_1_from + region_1_size; i++) {
+        SET_INTEGER_ELT(instance, i, region_1_value);
+    }
+
+    for (int i = region_2_from; i < region_2_from + region_2_size; i++) {
+        SET_INTEGER_ELT(instance, i, region_2_value);
+    }
+
+    int buf[3] = {0};
+    INTEGER_GET_REGION(instance, region_1_from, region_1_size, buf);
+    for (int i = 0; i < region_1_size; i++) {
+        CHECK( buf[i] == region_1_value);
+    }
+    INTEGER_GET_REGION(instance, region_2_from, region_2_size, buf);
+    for (int i = 0; i < region_2_size; i++) {
+        CHECK( buf[i] == region_2_value);
+    }
+
+end:
+    UNPROTECT(1);
 }
