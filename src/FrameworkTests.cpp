fc1d3b2d2bd83075485f76ce7da32e117c109f89
Pavel Marek <pavel.marek@oracle.com>
2020-06-12 16:37:49 +0200

    FrameworkTests do not use SimpleClass
    Instead, they create some dummy altrep class.

--- src/FrameworkTests.cpp
+++ src/FrameworkTests.cpp
@@ -1,6 +1,4 @@
 #include "FrameworkTests.hpp"
-#include "SimpleClass.hpp"
-#include "APIWrapper.hpp"
 
 const std::vector< Test> FrameworkTests::tests = {
     {"test_altrep_inheritance", testAltrepInheritance},
@@ -11,11 +9,29 @@ const std::vector< Test> FrameworkTests::tests = {
 };
 R_altrep_class_t FrameworkTests::simple_descr;
 
+static void * dummy_Dataptr(SEXP instance, Rboolean writeable) {
+    if (R_altrep_data1(instance) != R_NilValue) {
+        return DATAPTR(R_altrep_data1(instance));
+    }
+    else {
+        return nullptr;
+    }
+}
+
+static R_xlen_t dummy_Length(SEXP instance) {
+    if (R_altrep_data1(instance) != R_NilValue) {
+        return LENGTH(R_altrep_data1(instance));
+    }
+    else {
+        return 1;
+    }
+}
 
 void FrameworkTests::init(DllInfo *dll)
 {
-    SimpleClass::init(dll);
-    simple_descr = SimpleClass::getDescriptor();
+    simple_descr = R_make_altinteger_class("DummyClass", "AltrepTests", dll);
+    R_set_altrep_Length_method(simple_descr, dummy_Length);
+    R_set_altvec_Dataptr_method(simple_descr, dummy_Dataptr);
 }
 
 SEXP FrameworkTests::run()
@@ -27,7 +43,7 @@ SEXP FrameworkTests::run()
 TestResult FrameworkTests::testAltrepInheritance()
 {
     INIT_TEST;
-    SEXP instance = APIWrapper::R_new_altrep(simple_descr, R_NilValue, R_NilValue);
+    SEXP instance = R_new_altrep(simple_descr, R_NilValue, R_NilValue);
     CHECK( R_altrep_inherits(instance, simple_descr));
     FINISH_TEST;
 }
@@ -36,7 +52,7 @@ TestResult FrameworkTests::testAltrepInheritance()
 TestResult FrameworkTests::testStructHeader()
 {
     INIT_TEST;
-    SEXP instance = APIWrapper::R_new_altrep(simple_descr, R_NilValue, R_NilValue);
+    SEXP instance = R_new_altrep(simple_descr, R_NilValue, R_NilValue);
     CHECK_MSG( ALTREP(instance), "Instance has not ALTREP bit set");
     FINISH_TEST;
 }
@@ -45,16 +61,16 @@ TestResult FrameworkTests::testInstanceData()
 {
     INIT_TEST;
     // data1 = [42], data2 = NA
-	SEXP expected_instance_data1 = PROTECT(allocVector(INTSXP, 1));
+	SEXP expected_instance_data1 = PROTECT(Rf_allocVector(INTSXP, 1));
 	int *data_ptr = INTEGER(expected_instance_data1);
 	*data_ptr = 42;
 	SEXP expected_instance_data2 = R_NilValue;
 
-    SEXP instance = APIWrapper::R_new_altrep(simple_descr, expected_instance_data1, expected_instance_data2);
+    SEXP instance = R_new_altrep(simple_descr, expected_instance_data1, expected_instance_data2);
 
 	int default_flags = 16;
-	CHECK( R_compute_identical(APIWrapper::R_altrep_data1(instance), expected_instance_data1, default_flags));
-	CHECK( R_compute_identical(APIWrapper::R_altrep_data2(instance), expected_instance_data2, default_flags));
+	CHECK( R_compute_identical(R_altrep_data1(instance), expected_instance_data1, default_flags));
+	CHECK( R_compute_identical(R_altrep_data2(instance), expected_instance_data2, default_flags));
     UNPROTECT(1);
     FINISH_TEST;
 }
@@ -63,14 +79,14 @@ TestResult FrameworkTests::testModifyInstanceData()
 {
     INIT_TEST;
     // data1 will be VecInt[], ale will be set to VecInt[42]
-    SEXP int_vec = PROTECT(allocVector(INTSXP, 1));
-    SEXP instance = PROTECT(APIWrapper::R_new_altrep(simple_descr, int_vec, R_NilValue));
+    SEXP int_vec = PROTECT(Rf_allocVector(INTSXP, 1));
+    SEXP instance = PROTECT(R_new_altrep(simple_descr, int_vec, R_NilValue));
 
-    SET_INTEGER_ELT(APIWrapper::R_altrep_data1(instance), 0, 42);
+    SET_INTEGER_ELT(R_altrep_data1(instance), 0, 42);
 
-    SEXP actual_data1 = APIWrapper::R_altrep_data1(instance);
-    CHECK( isInteger(actual_data1));
-    CHECK( length(actual_data1) == 1);
+    SEXP actual_data1 = R_altrep_data1(instance);
+    CHECK( Rf_isInteger(actual_data1));
+    CHECK( LENGTH(actual_data1) == 1);
     CHECK( INTEGER_ELT(actual_data1, 0) == 42);
 
     UNPROTECT(2);
@@ -80,40 +96,32 @@ TestResult FrameworkTests::testModifyInstanceData()
 TestResult FrameworkTests::testSetInstanceData()
 {
     INIT_TEST;
-    SEXP instance = PROTECT(APIWrapper::R_new_altrep(simple_descr, R_NilValue, R_NilValue));
-    SEXP int_vec = PROTECT(ScalarInteger(42));
-    SEXP lgl_vec = PROTECT(ScalarLogical(1));
-    R_set_altrep_data1(instance, int_vec);
-    R_set_altrep_data2(instance, lgl_vec);
+    SEXP instance = PROTECT(R_new_altrep(simple_descr, R_NilValue, R_NilValue));
+    SEXP int_vec_1 = PROTECT(ScalarInteger(42));
+    SEXP int_vec_2 = PROTECT(ScalarInteger(23));
+    R_set_altrep_data1(instance, int_vec_1);
+    R_set_altrep_data2(instance, int_vec_2);
 
     int default_flags = 16;
-    CHECK( R_compute_identical(APIWrapper::R_altrep_data1(instance), int_vec, default_flags));
-    CHECK( R_compute_identical(APIWrapper::R_altrep_data2(instance), lgl_vec, default_flags));
+    CHECK( R_compute_identical(R_altrep_data1(instance), int_vec_1, default_flags));
+    CHECK( R_compute_identical(R_altrep_data2(instance), int_vec_2, default_flags));
 
     UNPROTECT(3);
     FINISH_TEST;
 }
 
 
-
-static void *dataptr_method_2(SEXP instance, Rboolean writeabble) {
-    return DATAPTR(R_altrep_data1(instance));
-}
-static R_xlen_t length_method_2(SEXP instance) { return 1; }
 /**
  * Tests that one DATAPTR call in AST should be able to return two different values.
  */
 TestResult FrameworkTests::testDifferentDataptrValue()
 {
     INIT_TEST;
-    R_altrep_class_t descr = R_make_altinteger_class("try_class", "try", nullptr);
-    R_set_altrep_Length_method(descr, &length_method_2);
-    R_set_altvec_Dataptr_method(descr, &dataptr_method_2);
 
     SEXP data1 = ScalarInteger(223);
     SEXP data2 = ScalarInteger(42);
-    SEXP instance1 = R_new_altrep(descr, data1, R_NilValue);
-    SEXP instance2 = R_new_altrep(descr, data2, R_NilValue);
+    SEXP instance1 = R_new_altrep(simple_descr, data1, R_NilValue);
+    SEXP instance2 = R_new_altrep(simple_descr, data2, R_NilValue);
 
     void *first_dataptr = nullptr;
     void *second_dataptr = nullptr;
